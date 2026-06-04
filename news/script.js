0dd8346a91c0858de136642963cb2b58aa0b1eb6
Shaikhanas2110 <145525463+Shaikhanas2110@users.noreply.github.com>
2026-06-04 20:45:10 +0530

    Update script.js

--- news/script.js
+++ news/script.js
@@ -15,6 +15,9 @@ async function fetchNews(query) {
   
 
 
+
+
+
 function bindData(articles) {
     const cardsContainer = document.getElementById("cards-container");
     const newsCardTemplate = document.getElementById("template-news-card");
