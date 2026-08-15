0d6350db63d2ee0d51890d95be78de8163aa8245
Shaikhanas2110 <145525463+Shaikhanas2110@users.noreply.github.com>
2026-08-15 21:25:40 +0530

    Update script.js

--- Text To Speech/script.js
+++ Text To Speech/script.js
@@ -1,9 +1,6 @@
 const sbt = document.getElementById("btn");
 const voiceSelect = document.getElementById("voice");
 const textArea = document.getElementById("textareaa");
-  
-  
-  
 
 function voiceList() {
   const voices = speechSynthesis.getVoices();
